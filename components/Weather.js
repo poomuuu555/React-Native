bf6c1e9551e36dfdde7f487ed101c9bef129d43b
poomuuu555 <poomuuu555@gmail.com>
2020-09-30 15:00:54 +0700

    Navigation & Route Parameter

---
+++ components/Weather.js
@@ -0,0 +1,67 @@
+import React , {useEffect, useState} from 'react';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import Forecast from './Forecast';
+
+const apiKey = '6cecb9dd2e369d7e9b5d62bc682150d4'
+
+export default function Weather(props) {
+
+    const [forecastInfo, setForecastInfo] = useState({
+        main: 'main',
+        description: 'description',
+        temp: 0
+    });
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {              
+   
+   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=${apiKey}`)
+                   .then((response) => response.json())
+                   .then((json) => {
+                       setForecastInfo({
+                           main: json.weather[0].main,
+                           description: json.weather[0].description,
+                           temp: json.main.temp
+                       });
+                   })
+                   .catch((error) => {
+                       console.warn(error);
+                   });
+           }
+       }, [props.zipCode])
+   
+
+    return (
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <View style={styles.cover}>
+                    <Text style={styles.medium}>Zip Code is {props.zipCode}</Text>
+                    <Forecast  style={styles.medium} {...forecastInfo}></Forecast>
+                </View>
+            </ImageBackground>
+        </View>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    backdrop:{
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+    cover: {
+        backgroundColor: 'black',
+        width: '100%',
+        height: 250,
+        opacity: 0.4,
+        alignItems: 'center',
+    },
+    medium: {
+        marginTop: 32,
+        fontSize: 15,
+        color: 'white',
+    }
+
+});
